f800106c04990bae354b8773fb63ef10804b9606
shubhambhawsar6 <98647679+shubhambhawsar6@users.noreply.github.com>
2022-01-30 14:55:01 +0530

    Pushing the variables program

---
+++ Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+
+    public static void main(String[] args) {
+        int a=5;
+        boolean b=false;
+        double c=2.5;
+        String str="Shubham";
+        char ch='A';
+        float f1=4.6F;
+        System.out.println(a+" "+b+" "+c+" "+str+" "+ch+" "+f1+" ");
+
+
+    }
+
+}
+
